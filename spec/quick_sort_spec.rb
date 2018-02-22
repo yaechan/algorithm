03b6dfd0511a4aafef8173e3a420aebe555abbda
yaegashi <yaegashi@indb.co.jp>
2018-02-22 15:31:08 +0900

    quick_sortのテスト追加（#swap, #swap!）

--- spec/quick_sort_spec.rb
+++ spec/quick_sort_spec.rb
@@ -3,9 +3,33 @@
 require "spec_helper"
 
 RSpec.describe Array do
+  let(:array) { [0, 1, 2, 3] }
+
   describe "#quick_sort" do
     it "returns sorted array" do
       expect([*1..9].shuffle.quick_sort).to eq [*1..9]
+    end
+  end
+
+  describe "#swap" do
+    it "returns swapped array" do
+      expect(array.swap(0, 2)).to eq [2, 1, 0, 3]
+    end
+
+    it "doesn't change self" do
+      array.swap(0, 2)
+      expect(array).to eq [0, 1, 2, 3]
+    end
+  end
+
+  describe "#swap!" do
+    it "returns swapped array" do
+      expect(array.swap!(0, 2)).to eq [2, 1, 0, 3]
+    end
+
+    it "changes self" do
+      array.swap!(0, 2)
+      expect(array).to eq [2, 1, 0, 3]
     end
   end
 end
